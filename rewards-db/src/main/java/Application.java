3e8e197cd66bc65deec712bef91c8b383a4aabb1
chamu-nune <chamu1930@hmail.com>
2024-04-11 09:47:12 -0500

    initial commit

---
+++ rewards-db/src/main/java/Application.java
@@ -0,0 +1,11 @@
+package rewards;
+
+
+@SpringBootApplication
+public class Application  {
+
+	public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+	}
+
+}
